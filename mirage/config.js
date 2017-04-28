4e45c9462696915b5c96c9326a2f5539cc4a7acb
Rik <gitaarik@gmail.com>
2017-04-28 14:52:21 +0200

    added chart edit page

--- mirage/config.js
+++ mirage/config.js
@@ -47,4 +47,72 @@ export default function() {
     };
   });
 
+  this.get('/charts/1', function() {
+    return {
+      data: {
+        type: 'charts',
+        id: 1,
+        attributes: {
+          title: "All of me",
+        },
+        relationships: {
+          sections: {
+            data: [
+              { type: 'section', id: 1 }
+            ]
+          }
+        }
+      },
+      included: [
+        {
+          type: 'section',
+          id: 1,
+          attributes: {
+            name: 'Intro'
+          },
+          relationships: {
+            lines: {
+              data: [
+                { type: 'line', id: 1 }
+              ]
+            }
+          }
+        },
+        {
+          type: 'line',
+          id: 1,
+          attributes: {},
+          relationships: {
+            measures: {
+              data: [
+                { type: 'measure', id: 1 }
+              ]
+            }
+          }
+        },
+        {
+          type: 'measure',
+          id: 1,
+          attributes: {
+            'beat-schema': '4'
+          },
+          relationships: {
+            chords: {
+              data: [
+                { type: 'chord', id: 1}
+              ]
+            }
+          }
+        },
+        {
+          type: 'chord',
+          id: 1,
+          attributes: {
+            name: 'C7'
+          }
+        }
+      ]
+    };
+  });
+
 }
